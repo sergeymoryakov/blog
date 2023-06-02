c7cd551b3f8d2ed5a7f386c442f05913dcf727b4
Sergey Moryakov <sergey@moryakov.com>
2023-06-02 12:07:47 +0300

    Add body and source inputs and render articles

--- blog.js
+++ blog.js
@@ -1,16 +1,69 @@
-let title = '';
+const articles = [];
+
+// {
+//     title: '',
+//     body: '',
+//     date: '',
+//     source: ''
+// }
 
 const titleInputNode = document.getElementById('titleInput');
+const bodyInputNode = document.getElementById('bodyInput');
+const sourceInputNode = document.getElementById('sourceInput');
 const postBtnNode = document.getElementById('postBtn');
 const articlesNode = document.getElementById('articles');
 
 postBtnNode.addEventListener('click', function () {
-    postTitle = titleInputNode.value;
+    // Get text from input field
+    const articleFromUser = getArticleFromUser();
 
-    articlesNode.innerText = postTitle;
+    // Save text
+    addArticle(articleFromUser);
 
-    console.log(postTitle);
+    // Render text
+    renderArticles();
 });
 
+function getArticleFromUser() {
+    const title = titleInputNode.value;
+    const body = bodyInputNode.value;
+    const source = sourceInputNode.value;
 
-console.log(title);
+    return {
+        title: title,
+        body: body,
+        source: source
+    };
+}
+
+function addArticle({ title, body, source }) {
+    articles.push({
+        title: title,
+        body: body,
+        source: source
+    });
+}
+
+function getArticles() {
+    return articles;
+}
+
+
+function renderArticles() {
+    const articles = getArticles();
+
+    let articlesHTML = '';
+
+    articles.forEach(article => {
+        articlesHTML += `
+            <div class='article'>
+                <p class='article-title'>${article.title}</p>
+                <p class='article-body'>${article.body}</p>
+                <p class='article-source'>${article.source}</p>
+            </div>        
+        `;
+    });
+
+    articlesNode.innerHTML = articlesHTML;
+}
+
