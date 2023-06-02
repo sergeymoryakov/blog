8aaf3366e6ea8d6f7d4f2ea97c09c9426357c01a
Sergey Moryakov <sergey@moryakov.com>
2023-06-02 16:50:15 +0300

    include articles rendering and characters counting

--- blog.js
+++ blog.js
@@ -8,38 +8,74 @@ const articles = [];
 // }
 
 const titleInputNode = document.getElementById('titleInput');
+const titleCounterNode = document.getElementById('titleCounter');
 const bodyInputNode = document.getElementById('bodyInput');
+const bodyCounterNode = document.getElementById('bodyCounter');
 const sourceInputNode = document.getElementById('sourceInput');
+const sourceCounterNode = document.getElementById('sourceCounter');
 const postBtnNode = document.getElementById('postBtn');
 const articlesNode = document.getElementById('articles');
+
+// Event listeners for title and body text counting
+titleInputNode.addEventListener('input', updateTitleCounter);
+bodyInputNode.addEventListener('input', updateBodyCounter);
+sourceInputNode.addEventListener('input', updateSourceCounter);
 
 postBtnNode.addEventListener('click', function () {
     // Get text from input field
     const articleFromUser = getArticleFromUser();
+
+    if (!articleFromUser.title || !articleFromUser.body || !articleFromUser.source) {
+        alert('Please enter all required firlds');
+        return;
+    }
 
     // Save text
     addArticle(articleFromUser);
 
     // Render text
     renderArticles();
+
+    // Clear input fields
+    titleInputNode.value = '';
+    bodyInputNode.value = '';
+    sourceInputNode.value = '';
 });
+
+function updateTitleCounter() {
+    const remainingChars = 100 - titleInputNode.value.length;
+    titleCounterNode.textContent = `${remainingChars} characters remaining`;
+}
+
+function updateBodyCounter() {
+    const remainingChars = 500 - bodyInputNode.value.length;
+    bodyCounterNode.textContent = `${remainingChars} characters remaining`;
+}
+
+function updateSourceCounter() {
+    const remainingChars = 100 - sourceInputNode.value.length;
+    sourceCounterNode.textContent = `${remainingChars} characters remaining`;
+}
 
 function getArticleFromUser() {
     const title = titleInputNode.value;
     const body = bodyInputNode.value;
+    const date = new Date();
     const source = sourceInputNode.value;
 
     return {
         title: title,
         body: body,
+        date: date,
         source: source
     };
 }
 
-function addArticle({ title, body, source }) {
+function addArticle({ title, body, date, source }) {
     articles.push({
         title: title,
         body: body,
+        date: date,
         source: source
     });
 }
@@ -51,19 +87,26 @@ function getArticles() {
 
 function renderArticles() {
     const articles = getArticles();
-
+    
     let articlesHTML = '';
 
-    articles.forEach(article => {
+    for (let i = articles.length - 1; i >= 0; i--) {
+        const article = articles[i];
         articlesHTML += `
             <div class='article'>
                 <p class='article-title'>${article.title}</p>
+                <p class='article-date'>${article.date.toUTCString()}</p>
                 <p class='article-body'>${article.body}</p>
                 <p class='article-source'>${article.source}</p>
-            </div>        
+            </div>
+            <hr class='article-divider'>
         `;
-    });
+    };
 
     articlesNode.innerHTML = articlesHTML;
 }
+
+updateTitleCounter();
+updateBodyCounter();
+updateSourceCounter();
 
